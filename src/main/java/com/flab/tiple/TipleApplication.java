235b14663ba8cb1b2dc99d47d933f572d3a052c6
sejin lee <79626319+sjleeJ@users.noreply.github.com>
2025-03-04 16:31:35 +0900

    feat:Initial Commit

---
+++ src/main/java/com/flab/tiple/TipleApplication.java
@@ -0,0 +1,13 @@
+package com.flab.tiple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/tiple/TipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.tiple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
