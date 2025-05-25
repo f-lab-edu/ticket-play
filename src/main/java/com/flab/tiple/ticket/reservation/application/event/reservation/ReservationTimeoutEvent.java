4675b4d09ecf4c4e55f2b6690b399f03e59a6108
sejin lee <79626319+sjleeJ@users.noreply.github.com>
2025-05-25 21:26:41 +0900

    feat:event공통화 및 추가, 기존 TicketReservationService코드 주석 추가

--- src/main/java/com/flab/tiple/ticket/reservation/application/event/reservation/ReservationTimeoutEvent.java
+++ src/main/java/com/flab/tiple/ticket/reservation/application/event/reservation/ReservationTimeoutEvent.java
@@ -1,25 +1,16 @@
 package com.flab.tiple.ticket.reservation.application.event.reservation;
 
-import com.flab.tiple.ticket.reservation.application.event.AbstractDomainEvent;
 import com.flab.tiple.ticket.reservation.domain.model.TicketReservation;
 
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-public class ReservationTimeoutEvent extends AbstractDomainEvent {
-	private final Long reservationId;
-	private final Long seatId;
-	private final Long memberId;
-	private final Long concertId;
+public class ReservationTimeoutEvent extends ReservationDomainEvent {
 
 	@Builder
 	public ReservationTimeoutEvent(Long reservationId, Long seatId, Long memberId, Long concertId) {
-		super();
-		this.reservationId = reservationId;
-		this.seatId = seatId;
-		this.memberId = memberId;
-		this.concertId = concertId;
+		super(reservationId, seatId, memberId, concertId);
 	}
 
 	public static ReservationTimeoutEvent from(TicketReservation reservation) {
---
+++ src/main/java/com/flab/tiple/ticket/reservation/application/notification/enums/NotificationStatus.java
@@ -0,0 +1,7 @@
+package com.flab.tiple.ticket.reservation.application.notification.enums;
+
+public enum NotificationStatus {
+	RETRYING,
+	SUCCESS,
+	PENDING
+}
